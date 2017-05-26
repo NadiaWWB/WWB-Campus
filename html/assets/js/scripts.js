8a8dff5960121fee03ec08b662fd1843d21776fb
Aaron Berkowitz <asberkowitz@pbs.org>
2017-05-26 15:28:03 -0400

    js changes

--- html/assets/js/scripts.js
+++ html/assets/js/scripts.js
@@ -236,7 +236,7 @@ function getButtonsFilterSelector(hashFilter){
 }
 ////////////////////////////////////////////////////
 //opens non-local links in new window
-// $('a:not([href^="http://wwb-campus.org"]):not([href^="#"]):not([href^="/"])').attr("target","_blank");
+$('a:not([href^="http://wwb-campus.org"]):not([href^="#"]):not([href^="/"])').attr("target","_blank");
 
   var menu = $('#navigation-menu');
 
